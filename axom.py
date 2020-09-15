6c107fc72614ae167a769061a4904ff6272b994d
Hamahackez <65807882+Hamahackez@users.noreply.github.com>
2020-09-16 01:04:25 +0300

    Update axom.py

--- axom.py
+++ axom.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python2
 # coding=utf-8
 
-import os,sys,time,datetime,random,hashlib,re,threading,json,getpass,urllib,cookielib
+import os,sheheys,time,datetime,random,hashlib,re,threading,json,getpass,urllib,cookielib
 from multiprocessing.pool import ThreadPool
 try:
 	import mechanize
